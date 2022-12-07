b64fcbfc0ac59f411cc0caaa7aead6fb1b70a262
milelogan <somtookoene65@gmail.com>
2022-12-07 13:50:02 +0100

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    '''
+        A function that traverse through a list for an element that matches
+        search and modify it with replace then returns a new list.
+        @elem: Elements
+    '''
     if len(my_list) == 0:
         return my_list
 
-    new_list = [element if element is not search else replace for element in my_list]
-    return new_list
+    new_lst = [elem if elem != search else replace for elem in my_list]
+    return new_lst
