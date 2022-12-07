8efe34ce261ab71e3f249657148be86b40a4aaf7
milelogan <somtookoene65@gmail.com>
2022-12-07 13:55:37 +0100

    task 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    '''
-        A function that traverse through a list for an element that matches
-        search and modify it with replace then returns a new list.
-        @elem: Elements
-    '''
+    """
+    A function that traverse through a list for an element that matches
+    search and modify it with replace then returns a new list.
+    @elem: Elements
+    """
     if len(my_list) == 0:
         return my_list
 
