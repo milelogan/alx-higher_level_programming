418df7ad6a4f58fb13ab03063698da90cbd73510
milelogan <somtookoene65@gmail.com>
2022-12-07 13:47:42 +0100

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) == 0:
+        return my_list
+
+    new_list = [element if element is not search else replace for element in my_list]
+    return my_list
