608be9b0bb0dfe8b04260ba3285720fc9f8697d8
milelogan <somtookoene65@gmail.com>
2022-12-08 06:45:22 +0100

    advanced

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num, check = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        check += tup[1]
+
+    return num / check
