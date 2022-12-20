7cd71b26962d4d03fe088df45191c4b6eae34e1a
milelogan <somtookoene65@gmail.com>
2022-12-20 17:45:17 +0100

    tired

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Check Again"""
+"""Creating square object again"""
 
 class Square:
     """Creating an empty square class"""
 
     def __init__(self, size):
-        """size is a private attribute"""
-	self.size = size
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+	self.__size = size
