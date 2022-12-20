6e5a47193fa62f45d0efe7392477da16cf3dda14
milelogan <somtookoene65@gmail.com>
2022-12-20 17:47:30 +0100

    tiredt

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Creating square object again"""
-
 class Square:
     """Creating an empty square class"""
 
     def __init__(self, size):
-        """Initializes a square
-        Args:
-            size (int): size of a side of the square
-        Returns: None
-        """
-	self.__size = size
+    """Initializes a square
+    Args:
+        size (int): size of a side of the square
+    Returns: None
+    """
+	self.size = size
