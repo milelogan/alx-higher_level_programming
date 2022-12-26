c7f686796eb14a6acccc363b7e25678af44c3b5c
milelogan <somtookoene65@gmail.com>
2022-12-26 14:06:50 +0100

    recheck

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-""" Function that adds two integers """
+"""
 
-def add_integer(a, b = 98):
-     """Return the sum of two integers or floats as integers
-     Args:
-         a: first argument
-         b: second argument
-     Returns:
-         Sum of the two arguments
-     Raises:
-         TypeError: If either of the arguments not an integer or a float
-     """
-	if type(a) is not in [int, float]:
-		raise TypeError("a must be an integer")
-	if type(b) is not in [int, float]:
-	    	raise TypeError("b must be an integer")
-	return a + b
+Function that adds two integers
+
+"""
+
+
+def add_integer(a, b=98):
+    """Return the sum of two integers or floats as integers
+
+    Args:
+        a: first argument
+        b: second argument
+
+    Returns:
+        Sum of the two arguments
+    Raises:
+        TypeError: If either of the arguments not an integer or a float
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return a + b
