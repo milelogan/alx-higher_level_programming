0a2a31ef88aed29cf2e8321c7b794fd7ee2ad1ca
milelogan <somtookoene65@gmail.com>
2023-01-02 12:25:42 +0100

    check all

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -67,3 +67,16 @@ class Rectangle:
             raise TypeError("Height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
