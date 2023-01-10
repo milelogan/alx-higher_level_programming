58bd85b27a760e253189dbff0812d59dff534b52
milelogan <somtookoene65@gmail.com>
2023-01-10 18:21:04 +0100

    write a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" write a file"""
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """write a file that doesn't exist and overrite"""
-    with open(filename, mode="w", encode="utf-8") as f:
+    """Write a string to a UTF"""
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
