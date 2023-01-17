e619f5c1b5ab412354607740202f76689ca86fd5
milelogan <somtookoene65@gmail.com>
2023-01-17 06:22:23 +0100

    check

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,7 +37,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns list of json string"""
-        return json.loads(json_string or [])
+        return json.loads(json_string or '[]')
 
     @classmethod
     def create(cls, **dictionary):
