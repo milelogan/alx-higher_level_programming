11cbe217df96fc0a435e79059bd1375e6a0dec0a
milelogan <somtookoene65@gmail.com>
2023-02-14 22:42:09 +0100

    spelt order wrong

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists score above or equal to 10 in table 2
 
-SELECT score, name FROM second_table WHERE score >= 10 OORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
