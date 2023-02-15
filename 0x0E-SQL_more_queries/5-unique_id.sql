ea55781fea6fa2831967c61025ea0f04ede95678
milelogan <somtookoene65@gmail.com>
2023-02-15 20:49:05 +0100

    5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
---create a table unique_id on your mysql server
---creates a table
+-- creates the table unique_id on your MySQL server
+-- creates a table
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
