152b2d48b68e61d6bcd7ff51c47f242f3ef56613
milelogan <somtookoene65@gmail.com>
2023-03-13 15:50:42 +0100

    school

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // a script that prints the first argument passed to it.
 
 if (process.argv[2] !== undefined) {
-	console.log(process.argv[2]);
+  console.log(process.argv[2]);
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
