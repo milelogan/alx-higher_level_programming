c5987100f1a7b44e952e8534c0caf914a0b8a9a8
milelogan <somtookoene65@gmail.com>
2023-03-13 16:00:32 +0100

    check

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script to number
+
+let args = process.argv.slice(2);
+if (isNaN(Number(args[0]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(args[0]));
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// still on loops
+
+let args = process.argv[2];
+if (isNaN(Number(args))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(args); i++) {
+    console.log('C is fun');
+  }
+}
