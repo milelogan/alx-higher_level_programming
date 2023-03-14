bee2c903f14321d0042af35e2d6f25e10bdcd762
milelogan <somtookoene65@gmail.com>
2023-03-14 19:43:44 +0100

    check

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,25 +1,6 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () { [this.width, this.height] = [this.height, this.width]; }
-  double () { this.width *= 2; this.height *= 2; }
-}
-
-class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle') {
   constructor (size) {
     super(size, size);
   }
-}
-
-module.exports = Square;
+};
